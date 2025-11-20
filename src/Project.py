b3e5b40759db9193332903cad16ea974ac9bb0d9
Ziad-Belal <ziadbelal82@gmail.com>
2025-11-20 14:10:13 +0200

    edited the relative paths in the connection of the daaset with the model.

--- src/Project.py
+++ src/Project.py
@@ -1,6 +1,6 @@
 import pandas as panda
-All_players=panda.read_csv('All_Players.csv')
-season=panda.read_csv('Season.csv')
+All_players=panda.read_csv('data\All_Players.csv')
+season=panda.read_csv('data\Season.csv')
 print(All_players.columns)
 print(season.columns)
 
